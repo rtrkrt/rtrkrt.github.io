726474045167f0704860f2c2a58e14a56fb2a6e9
rtrkrt <61792318+rtrkrt@users.noreply.github.com>
2020-04-23 07:56:49 +0200

    Create adles.mm

---
+++ adles.mm
@@ -0,0 +1,9 @@
+#### Aus dem Leben eines Spitzenprädators
+
+_20. Oktober 2019_
+
+Ich: "Ich möchte nicht mehr 'kämpfende Löwen' spielen!"<br>
+Kind: "Dann spielen wir 'Batman kämpft gegen Löwen' ..."<br>
+Ich: "Nagut."<br>
+Kind: "... und Batman kann sich in einen Löwen verwandeln!"<br>
+Ich: "Nagut."
